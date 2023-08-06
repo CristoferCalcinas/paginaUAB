3c845ba6f2083cdc76a749226989d1993b1745ca
adrian <adriapruebadespliegue@gmaile.com>
2023-08-06 14:45:41 -0400

    Complementario TERMINADO

--- src/pages/UAB/uabSecundario/Complementario/ComplementarioContent.jsx
+++ src/pages/UAB/uabSecundario/Complementario/ComplementarioContent.jsx
@@ -1,10 +1,16 @@
+import { LayoutCard } from "../LayoutCard/LayoutCard";
+import { COMPLEMENTARIO } from "./const/complementario";
 
 export const ComplementarioContent = () => {
+  const complementario = COMPLEMENTARIO;
   return (
     <div className="overflow-hidden h-full bg-white">
       <section className="text-gray-600">
         <section className="text-gray-600">
-          <LayoutCard layoutInfo={postgrado} titleLayout={"Postgrado UAB"} />
+          <LayoutCard
+            layoutInfo={complementario}
+            titleLayout={"Complementario UAB"}
+          />
         </section>
       </section>
     </div>
--- src/pages/UAB/uabSecundario/Complementario/const/complementario.js
+++ src/pages/UAB/uabSecundario/Complementario/const/complementario.js
@@ -1,3 +1,5 @@
 export const COMPLEMENTARIO = [
-    {},
+    { id: 1, title: "Complementario en Administración y Negocios Internacionales", img: "src/pages/UAB/uabSecundario/Complementario/image/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
+    { id: 2, title: "Complementario en Contaduría Pública", img: "src/pages/UAB/uabSecundario/Complementario/image/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
+    { id: 3, title: "Complementario en Ingeniería de Sistemas", img: "src/pages/UAB/uabSecundario/Complementario/image/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
 ];
