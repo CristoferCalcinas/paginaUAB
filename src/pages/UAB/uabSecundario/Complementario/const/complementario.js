ff4064ce71d2192f8f997ec231fd79351bb74ab4
adrian <adriapruebadespliegue@gmaile.com>
2023-08-08 19:31:50 -0400

    Finalizando la pagina Complementario con el bug de imagenes

--- src/pages/UAB/uabSecundario/Complementario/const/complementario.js
+++ src/pages/UAB/uabSecundario/Complementario/const/complementario.js
@@ -1,5 +1,5 @@
 export const COMPLEMENTARIO = [
-    { id: 1, title: "Complementario en Administración y Negocios Internacionales", img: "src/pages/UAB/uabSecundario/Complementario/image/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
-    { id: 2, title: "Complementario en Contaduría Pública", img: "src/pages/UAB/uabSecundario/Complementario/image/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
-    { id: 3, title: "Complementario en Ingeniería de Sistemas", img: "src/pages/UAB/uabSecundario/Complementario/image/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
+    { id: 1, title: "Complementario en Administración y Negocios Internacionales", img: "/imagesLetras/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
+    { id: 2, title: "Complementario en Contaduría Pública", img: "/imagesLetras/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
+    { id: 3, title: "Complementario en Ingeniería de Sistemas", img: "/imagesLetras/C.jpg", level: "Nivel de egreso: Licenciatura Duración: 2 Años Requisito: Técnico Superior." },
 ];
