13c023154dd8ba77cc1e6fe81667143ab513a12a
adrian <adriapruebadespliegue@gmaile.com>
2023-08-08 19:05:26 -0400

    Comentando el Scroll horizontal

--- src/pages/UAB/uabPrincipal/UabContennt.jsx
+++ src/pages/UAB/uabPrincipal/UabContennt.jsx
@@ -3,6 +3,7 @@ import { SideBar } from "../../SideBar/SideBar";
 export const UabContennt = ({ children }) => {
   return (
     <div>
+      {/* Use el w-full para que se quitara el scrol horizontal */}
       <div className="flex flex-row h-full w-full bg-[#003b71] font-serif">
         <div className="flex flex-col w-1/5 bg-[#003264] items-center">
           <SideBar />
