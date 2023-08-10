494180beab638363bac88e509c33ad665be1083f
adrian <adriapruebadespliegue@gmaile.com>
2023-08-10 10:39:55 -0400

    Inicio de Layout de las Carreras

--- src/ContentMain/TargetMain/TargetMain.jsx
+++ src/ContentMain/TargetMain/TargetMain.jsx
@@ -1,11 +1,8 @@
 export const TargetMain = () => {
   return (
     <div className="container p-5 flex lg:px-5 lg:py-24 mx-auto lg:flex-wrap">
-      
       <div className="flex flex-col-reverse items-center  lg:flex-row lg:items-end lg:flex-wrap md:-m-2 -m-1">
         <div className="flex flex-wrap w-full lg:w-1/2 ">
-
-
           <div className="md:p-2 p-1 w-1/2 bg-gradient-to-r from-transparent to-[#f2295b]">
             <img
               alt="gallery"
@@ -13,9 +10,7 @@ export const TargetMain = () => {
               src="imagesParaMain/carreras.jpg"
             />
           </div>
-          
-          
-          
+
           <div className="md:p-2 p-1 w-1/2 bg-gradient-to-r from-transparent to-[#000284]">
             <img
               alt="gallery"
@@ -34,12 +29,12 @@ export const TargetMain = () => {
 
         <div className="flex flex-wrap w-full lg:w-1/2 ">
           <div className="md:p-2 p-1 w-full flex flex-col justify-center text-white h-1/2">
-
-
             <h2 className="m-5 text-4xl lg:m-0 font-bold lg:text-6xl lg:text-ellipsis lg:text-end">
               Profesionales con Valores
             </h2>
-            <h3 className="uppercase mb-5 mr-5 lg:m-5 text-end">PROFESIONALES UAB</h3>
+            <h3 className="uppercase mb-5 mr-5 lg:m-5 text-end">
+              PROFESIONALES UAB
+            </h3>
           </div>
 
           <div className="md:p-2 p-1 w-1/2 bg-gradient-to-r from-[#840037] to-[#00748e]">
@@ -58,7 +53,6 @@ export const TargetMain = () => {
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
